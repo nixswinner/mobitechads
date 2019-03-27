8fb3ff0beb455637326a16616467cc328fa9baf9
Nixon <nixswinner@gmail.com>
2019-03-27 10:50:27 -0400

    add static on getAdsUrl method

--- app/src/main/java/com/ads/mobitechads/MainActivity.java
+++ app/src/main/java/com/ads/mobitechads/MainActivity.java
@@ -3,11 +3,16 @@ package com.ads.mobitechads;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ads.mobitechadslib.BannerAds;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BannerAds.ge
     }
 }
