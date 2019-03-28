e07806c32f1ce0cc6e153d2f0bcca0983d09c7ca
nixswinner <nixondeveloper@gmail.com>
2019-03-28 13:58:39 +0300

    add dialog to show intertistial

--- app/src/main/java/com/ads/mobitechads/MainActivity.java
+++ app/src/main/java/com/ads/mobitechads/MainActivity.java
@@ -1,10 +1,16 @@
 package com.ads.mobitechads;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.ads.mobitechadslib.BannerAds;
+import com.ads.mobitechadslib.TestClass;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("Banner Ad Url", "onCreate: "+BannerAds.getAdsURL(MainActivity.this,"1"));
+        Log.e("Banner Ad Url", "onCreate: "+
+                BannerAds.getAdsURL(MainActivity.this,"1"));
+        /*TestClass.alertDialog(MainActivity.this);
+        TestClass.getButtonAction(MainActivity.this);*/
+
+        MyDialog(MainActivity.this);
+
+    }
+
+
+    public static void MyDialog(Activity activity){
+        Dialog dialog = new Dialog(activity,
+                android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        dialog.setContentView(R.layout.show_dialog);
+        dialog.setCancelable(true);
+        ImageView image = dialog.findViewById(R.id.imageView);
+        ImageView imgCancle = dialog.findViewById(R.id.cancle);
+        imgCancle.setOnClickListener(v->{
+            dialog.dismiss();
+        });
+        dialog.show();
 
     }
 }
--- mobitechadslib/src/main/java/com/ads/mobitechadslib/BannerAds.java
+++ mobitechadslib/src/main/java/com/ads/mobitechadslib/BannerAds.java
@@ -1,14 +1,24 @@
 package com.ads.mobitechadslib;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class BannerAds {
+    static AlertDialog.Builder alertDialog;
+    public static void alertDialog(Activity activity){
+
+        alertDialog.show();
+    }
     static AdsModel adsModel = new AdsModel();
     public static String getAdsURL(Activity activity,String categoryId) {
         Server.getInstance(new OnProgressListener() {
@@ -55,5 +65,73 @@ public class BannerAds {
             }
         }).request("http://ads.mobitechtechnologies.com/api/serve_ads/"+categoryId);
         return adsModel.getAd_upload();
+    }
+
+
+    public static String getIntertistial(Activity activity,String categoryId) {
+        Server.getInstance(new OnProgressListener() {
+            @Override
+            public void onFailure() {
+                activity.runOnUiThread( () -> { });
+            }
+            @Override
+            public void onResponse(final String response, final boolean success) {
+                activity.runOnUiThread(() -> {
+                    if (success) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.getJSONObject("fetch_status").get("response").equals("0")){
+                                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                                JSONObject banner = (JSONObject) jsonArray.get(0);
+                                JSONObject interstitial = (JSONObject) jsonArray.get(1);
+                                adsModel.setAd_name(String.valueOf(banner.get("ad_name")));
+                                adsModel.setAd_upload(String.valueOf(banner.get("ad_upload")));
+                                adsModel.setAd_urlandroid(String.valueOf(banner.get("ad_urlandroid")));
+                                adsModel.setAd_urlios(String.valueOf(banner.get("ad_urlios")));
+                                adsModel.setInterstitial_upload(String.valueOf(interstitial.get("interstitial_upload")));
+                                adsModel.setInterstitial_urlandroid(String.valueOf(interstitial.get("interstitial_urlandroid")));
+                                adsModel.setInterstitial_urlios(String.valueOf(interstitial.get("interstitial_urlios")));
+
+                                Log.i("tag", "<======= ==========>");
+                                Log.i("tag", "run: "+adsModel.toString());
+                                Log.i("tag", "<======= ==========>");
+                                //show intertistial
+                                showIntertistial(activity,adsModel.getAd_upload());
+
+                                //Glide.with(getApplicationContext()).load(adsModel.getAd_upload()).into(imageView);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+
+                });
+            }
+
+            @Override
+            public void onStart() {
+            }
+        }).request("http://ads.mobitechtechnologies.com/api/serve_ads/"+categoryId);
+        return adsModel.getAd_upload();
+    }
+
+    public static void showIntertistial(Activity activity,String ad_imageUrl){
+        Dialog dialog = new Dialog(activity,
+                android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        dialog.setContentView(R.layout.intertistial_dialog);
+        dialog.setCancelable(true);
+        ImageView image = dialog.findViewById(R.id.imageView);
+        ImageView imgCancle = dialog.findViewById(R.id.cancle);
+
+        //show image
+        Glide.with(activity)
+                .load(ad_imageUrl).into(image);
+
+        imgCancle.setOnClickListener(v->{
+            dialog.dismiss();
+        });
+        dialog.show();
     }
 }
