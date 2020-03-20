ccabf7a7694b88c21574973fc476ee52a37ac963
nixswinner <nixswinner@gmail.com>
2020-03-20 04:52:02 +0300

    work on adding video ad

--- mobitechadslib/src/main/java/com/ads/mobitechadslib/MobitechAds.java
+++ mobitechadslib/src/main/java/com/ads/mobitechadslib/MobitechAds.java
@@ -123,16 +123,10 @@ public class MobitechAds {
                 });
     }
     private static  void populateAdsList(Ads adList,Activity activity){
-        if(adList.getVideo()!=null){
-
-        }
         if(adList.getInterstitial_upload()!=null){
             showIntertistial(activity, adList.getInterstitial_upload(),
                     adList.getInterstitial_urlandroid());
         }
-        showVideoAd(activity, adList.getInterstitial_upload(),
-                adList.getInterstitial_urlandroid());
-
     }
     // show intertistial ad
     public static Dialog showIntertistial(final Activity activity, String ad_imageUrl,
@@ -169,6 +163,105 @@ public class MobitechAds {
         adLoaded=true;
         return dialog;
     }
+
+    //.........video ad ---------
+
+    //load video ad
+    public static void loadVideoAd(final Activity activity, final String applicationId,
+                                   final String categoryId) {
+       ApiService.ApiServicePublicIPAddress.Companion.create()
+                .getMyPublicIPAddress("json")
+                .enqueue(new Callback<PublicIP>() {
+                    @Override
+                    public void onResponse(Call<PublicIP> call, Response<PublicIP> response) {
+                        if (response.isSuccessful()){
+                            getVideoAdsForARegion(activity,applicationId,categoryId,response.body().getIp());
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<PublicIP> call, Throwable t) {
+                        Log.e("Mobitech Ads Error",
+                                "You do not have an active Internet connection");
+                    }
+                });
+    }
+
+    private static void getVideoAdsForARegion(
+            final Activity activity,
+            final String applicationId,
+            final String categoryId, final String device_ip) {
+
+        if(SaveSharedPreference.getIpAddress(activity).equals(device_ip)){
+            showIntertistialAd(categoryId,applicationId,
+                    SaveSharedPreference.getCountryCode(activity),
+                    activity);
+        }else {
+            AppLocationByIp.getInstance(activity).getAppLocViaIp(device_ip)
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(Schedulers.io())
+                    .subscribe(new Observer<UserLoc>() {
+                        @Override
+                        public void onSubscribe(Disposable d) {
+                        }
+                        @Override
+                        public void onNext(UserLoc userLoc) {
+                            SaveSharedPreference.setIpAddress(activity, userLoc.getIp());
+                            SaveSharedPreference.setCountryCode(activity, userLoc.getCountry_code());
+                            SaveSharedPreference.setCountryName(activity, userLoc.getCountry_name());
+                            SaveSharedPreference.setRegionName(activity, userLoc.getRegion_name());
+                            showIntertistialAd(categoryId,applicationId,
+                                    SaveSharedPreference.getCountryCode(activity),
+                                    activity);
+                        }
+                        @Override
+                        public void onError(Throwable e) {
+                            Log.e("Error ", "" + e.getMessage());
+                        }
+                        @Override
+                        public void onComplete() {
+                        }
+                    });
+        }
+    }
+
+    private static void showVideoAd(String categoryId,
+                                    final String applicationId,
+                                    final String country_code,
+                                    final Activity activity){
+        ApiService.Companion.create()
+                .getAds(categoryId,applicationId,
+                        country_code)
+                .enqueue(new Callback<AdsResult>() {
+                    @Override
+                    public void onResponse(Call<AdsResult> call,
+                                           retrofit2.Response<AdsResult> response) {
+                        if (response.isSuccessful()){
+                            Log.i("Mobitech VideoAd","Ad Loaded successfully ");
+                            adsList=response.body().getData();
+                            if (response.body().getData() !=null){
+                                populateVideoAdsList(response.body().getData(),activity);
+                                AppUsageDetails.getInstance(activity,applicationId,country_code);
+                            }
+
+                        }else {
+                            Log.e("Mobitech VideoAd","Ad Failed to Load ");
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<AdsResult> call, Throwable t) {
+                        Log.e("Mobitech VideoAd","Ad Could not load.No Internet Connections");
+                    }
+                });
+    }
+
+    private static  void populateVideoAdsList(Ads adList,Activity activity){
+        if(adList.getVideo()!=null){
+            showVideoAd(activity, adList.getVideo(),
+                    adList.getInterstitial_urlandroid());
+        }
+    }
+
+    //-------end video ad.--------
     public static Dialog showVideoAd(final Activity activity, String ad_imageUrl,
                                           final String click_url_redirect){
         final Dialog dialog = new Dialog(activity,
@@ -211,20 +304,6 @@ public class MobitechAds {
         String countryCodeValue = tm.getNetworkCountryIso();
         return countryCodeValue.toUpperCase();
     }
-
-    /*public  boolean setAdsStatus(boolean isloaded){
-        return isloaded;
-    }
-
-    public Dialog showIntertistialAd(){
-        if (adLoaded!=true){
-
-        }else {
-
-        }
-    }*/
-    //banner ads .....
-
 
 
 }
